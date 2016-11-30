1bc89fcdbf01ae08e5fb2669cb864d8b4b4fa783
cat_in_136 <cat.in.136+github@gmail.com>
2016-12-01 00:52:42 +0900

    enhance HTML layout

--- export_skype_chat_log_to_html.rb
+++ export_skype_chat_log_to_html.rb
@@ -16,24 +16,54 @@ class ExportSkypeChatLogToHTML
       keys = "id,chatname,author,from_dispname,timestamp,type,body_xml,chatmsg_type"
       cursor = (chat_room.nil?)? db.execute("SELECT #{keys} FROM Messages ORDER BY timestamp ASC") :
                                  db.execute("SELECT #{keys} FROM Messages WHERE chatname LIKE ? ORDER BY timestamp ASC", [chat_room])
-      p cursor.length
+      output << <<EOD
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8" />
+<title>skype chat log</title>
+<meta name="robots" content="noindex,nofollow" />
+<style type="text/css">
+body {
+  background: #fff;
+  color: #000;
+}
+.timestamp, .author, .message p {
+  display: inline;
+}
+.timestamp {
+  color: gray;
+}
+.author {
+  color: #00AFF0;
+  font-weight: bold;
+}
+</style>
+</head>
+<body>
+EOD
 
       cursor.each do |tuple|
         tuple_data_attrs = []
         tuple.each do |key,value|
-          unless key.to_s == /^([0-9]*|body_xml)$/
+          unless key.to_s =~ /^([0-9]*|body_xml)$/
             tuple_data_attrs << "data-#{key.to_s.gsub('_','-')}=\"#{CGI::escapeHTML(value.to_s)}\""
           end
         end
 
         output << "<div class=\"message\" #{tuple_data_attrs.join(' ')}>\n"
-        output << "<div class=\"author\">#{CGI::escapeHTML(tuple['author'].to_s)}</div>\n"
-        output << "<div class=\"timestamp\">#{Time.at(tuple['timestamp'].to_i)}</div>\n"
+        output << "<div class=\"timestamp\">[#{Time.at(tuple['timestamp'].to_i)}]</div>\n"
+        output << "<div class=\"author\">#{CGI::escapeHTML(tuple['author'].to_s)}:</div>\n"
         output << "<p>\n"
-        output << tuple["body_xml"]
+        output << tuple["body_xml"].to_s.gsub("\n", "<br />\n")
         output << "</p>\n"
         output << "</div>\n\n"
       end
+
+      output << <<EOD
+</body>
+</html>
+EOD
     end
   end
 
